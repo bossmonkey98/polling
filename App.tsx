bc8f35b71f881bb22a0ebd97b743348e1411ed39
bossmonkey98 <47918140+bossmonkey98@users.noreply.github.com>
2022-10-11 14:12:24 +0530

    [feat] Screen routes added

--- App.tsx
+++ App.tsx
@@ -1,20 +1,29 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { s } from 'react-native-wind'
+import Home from './Screens/Home';
+import RawJson from './Screens/RawJson';
+
+export type ScreenParamTypes = {
+  Home: undefined;
+  RawJSON: {data:any};
+}
 
 export default function App() {
+  const Stack = createNativeStackNavigator<ScreenParamTypes>()
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={s`h-full`}>
+        <StatusBar style="auto" />
+        <Stack.Navigator initialRouteName='Home'>
+          <Stack.Screen name='Home' component={Home} options={{ title: 'Home' }} />
+          <Stack.Screen name='RawJSON' component={RawJson} options={{ title: 'Raw JSON' }} />
+        </Stack.Navigator>
+      </View>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
